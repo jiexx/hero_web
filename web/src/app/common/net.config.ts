b165c1d38b975429f8867f4391f2a5ac76d632c8
yw <yw>
2019-12-26 00:39:03 +0800

    ip to domain

--- web/src/app/common/net.config.ts
+++ web/src/app/common/net.config.ts
@@ -4,16 +4,16 @@
 export class ConfigService {
     public MEDIA_HOST = {
        /*  URL: "http://49.234.15.176:8999/" */
-        URL: 'http://127.0.0.1:8999/'
-        /* URL: 'http://app.justitbe.com/' */
+        /* URL: 'http://127.0.0.1:8999/' */
+        URL: 'http://app.justitbe.com:8999/'
     };
     public REST_HOST = {
         /* URL: "http://49.234.15.176:8999/" */
-        URL: "http://localhost:8999/"
-        /* URL: 'http://app.justitbe.com/' */
+        /* URL: "http://localhost:8999/" */
+        URL: 'http://app.justitbe.com:8999/'
     }
     public FONT_HOST = {
-        URL: "http://49.234.15.176:8999/css/fonts.css"
+        URL: "http://app.justitbe.com:8999/css/fonts.css"
         /* URL: "http://localhost:8999/css/fonts.css" */
     }
     public __DEV__: boolean = true;
